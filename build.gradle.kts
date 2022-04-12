41b34c9ec7afc22fdd59b2ddc39671619a611299
Alex Rune Berg <arberg@gmail.com>
2022-04-12 20:37:42 +0200

    Switch to Kotlin 1.6.20 and coroutines 1.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 
 plugins {
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.6.20"
 }
 
-val kotlinVersion: String by extra("1.6.0")
-val kotlinCoroutinesVersion: String by extra("1.6.0-RC")
+val kotlinVersion: String by extra("1.6.20")
+val kotlinCoroutinesVersion: String by extra("1.6.1")
 
 allprojects {
     repositories {
