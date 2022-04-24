625831ab47860d2240ee309402e0ecc55620486b
vstashuk <vstashuk@google.com>
2022-04-24 17:03:03 +0200

    Demonstrate regression introduced in Kotlin Compiler v1.6.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("jvm") version "1.6.0"
 }
 
-val kotlinVersion: String by extra("1.6.0")
+val kotlinVersion: String by extra("1.6.20")
 val kotlinCoroutinesVersion: String by extra("1.6.0-RC")
 
 allprojects {
--- jvm/simple-main-kts/simple-main-kts-test/testData/import-test.smain.kts
+++ jvm/simple-main-kts/simple-main-kts-test/testData/import-test.smain.kts
@@ -5,3 +5,18 @@
 sharedVar = sharedVar + 1
 
 println("sharedVar == $sharedVar")
+
+fun someFun() {
+  // Using `sharedVar` inside a function breaks compiler (v1.6.20, v1.6.21):
+  // File being compiled: testData/import-test.smain.kts
+  // The root cause java.lang.RuntimeException was thrown at:
+  // org.jetbrains.kotlin.backend.jvm.codegen.FunctionCodegen.generate(FunctionCodegen.kt:50):
+  // org.jetbrains.kotlin.backend.common.BackendException: Backend Internal error: Exception during
+  // IR lowering
+  //
+  // It worked fine in v1.6.10.
+  //
+  // To reproduce:
+  // ./gradlew :jvm:simple-main-kts:simple-main-kts-test:clean :jvm:simple-main-kts:simple-main-kts-test:test
+  println(sharedVar)
+}
