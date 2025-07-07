d94574bcd6bb4d01f424bb5c136a38f6106269c9
vladislav.koshkin <vladislav.koshkin@jetbrains.com>
2025-07-07 18:40:52 +0200

    Add script documentation for kotlin-shell and kotlinx-html

--- jvm/main-kts/scripts/kotlinx-html.main.kts
+++ jvm/main-kts/scripts/kotlinx-html.main.kts
@@ -1,5 +1,17 @@
 #!/usr/bin/env kotlin
 
+/**
+ * A demonstration script showing HTML generation using kotlinx.html DSL.
+ * This script creates and prints a simple HTML document with a "Hello, World!" heading.
+ *
+ * Usage:
+ * - Run the script to generate HTML output
+ * - No command line arguments needed
+ *
+ * Dependencies:
+ * - kotlinx-html-jvm:0.8.0
+ */
+
 @file:DependsOn("org.jetbrains.kotlinx:kotlinx-html-jvm:0.8.0")
 
 import kotlinx.html.*
@@ -12,4 +24,3 @@ print(createHTML().html {
         h1 { +"Hello, $addressee!" }
     }
 })
-
